36297579eccc7f0a280addf76f79556d4f1e3bbb
rithik007 <52231735+rithik007@users.noreply.github.com>
2023-06-05 19:26:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT ,SUPPORT_CHAT_ID
 from utils import temp
